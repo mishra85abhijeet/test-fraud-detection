e013f4bbdce2e00d98e49b2e518a27d595132218
mishra85abhijeet <mishra85abhijeet@gmail.com>
2023-05-02 15:10:10 +0530

    ML model addition

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print('adding all ml model related code')
+
+def model1():
+    print('adding code for ml model1')
+
+def model2():
+    print('adding code for ml model2')
+
+def model3():
+    print('adding code for ml model3')
